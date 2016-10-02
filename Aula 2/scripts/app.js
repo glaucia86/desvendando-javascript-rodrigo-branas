2464022d6038d1852029a3d9d2ba74eb26bc15e0
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-10-02 19:57:26 -0300

    Inclusão da Aula 2 - Number

---
+++ Aula 2/scripts/app.js
@@ -0,0 +1,14 @@
+/**
+ * Arquivo: app.js
+ * Descrição: Aula introdutória com o objetivo de destacar os conceitos básicos sobre JavaScript.
+ *  Nesse primeiro exemplo estaremos desenvolvendo um 'Hello World.
+ * Author: Glaucia Lemos
+ * Data: 20/09/2016
+ */
+
+function greetMe(nome) {
+    //return "Olá, " + nome;
+    alert("Olá " + nome);
+}
+
+greetMe("mundo"); //"Olá, Mundo!"
